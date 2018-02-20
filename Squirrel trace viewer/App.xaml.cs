858f41e4b11c2806d9e78ef53b2e4673bf447ed9
brliron <brliron@hotmail.fr>
2018-02-20 17:00:49 +0100

    Add Squirrel trace viewer
    It's an empty XAML project for now.

---
+++ Squirrel trace viewer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Squirrel_trace_viewer
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
